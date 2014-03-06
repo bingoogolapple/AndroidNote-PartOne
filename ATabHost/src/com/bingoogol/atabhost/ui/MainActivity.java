ca7973cbaf01288c682bc5255441ce5e936a3353
bingoogol <bingoogol@sina.com>
2014-03-07 02:40:14 +0800

    Initial commit

---
+++ ATabHost/src/com/bingoogol/atabhost/ui/MainActivity.java
@@ -0,0 +1,23 @@
+package com.bingoogol.atabhost.ui;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+import com.bingoogol.atabhost.R;
+
+public class MainActivity extends TabActivity {
+    private TabHost mTabHost;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTabHost = getTabHost();
+
+        mTabHost.addTab(mTabHost.newTabSpec("tabOne").setIndicator("one").setContent(new Intent(this, OneActivity.class)));
+        mTabHost.addTab(mTabHost.newTabSpec("tabTwo").setIndicator("two").setContent(new Intent(this, TwoActivity.class)));
+        mTabHost.addTab(mTabHost.newTabSpec("tabThree").setIndicator("three").setContent(new Intent(this, ThreeActivity.class)));
+    }
+}
---
+++ ATabHost/src/com/bingoogol/atabhost/ui/OneActivity.java
@@ -0,0 +1,16 @@
+package com.bingoogol.atabhost.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.bingoogol.atabhost.R;
+
+public class OneActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
