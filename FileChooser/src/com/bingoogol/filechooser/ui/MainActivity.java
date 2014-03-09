893f4cd851cb7db986b5490ff3ee3c3f04b4a920
bingoogol <bingoogol@sina.com>
2014-03-10 04:06:39 +0800

    添加文件选择器

--- FileChooser/src/com/bingoogol/filechooser/ui/MainActivity.java
+++ FileChooser/src/com/bingoogol/filechooser/ui/MainActivity.java
@@ -2,50 +2,37 @@ package com.bingoogol.filechooser.ui;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 import com.bingoogol.filechooser.R;
-import com.bingoogol.filechooser.util.FileUtils;
+import com.bingoogol.filechooser.util.Constants;
 
 public class MainActivity extends Activity {
-    private static final int FILE_SELECT_CODE = 2;
+    private static final String TAG = "MainActivity";
+    private TextView tv_main_result;
 
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_main_result = (TextView) findViewById(R.id.tv_main_result);
     }
-
 
     public void showFileChooser(View view) {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/*");
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-        try {
-            startActivityForResult( Intent.createChooser(intent, "Select a File to Upload"), FILE_SELECT_CODE);
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(this, "Please install a File Manager.", Toast.LENGTH_SHORT).show();
-        }
+        Intent intent = new Intent(this, FileChooserActivity.class);
+        startActivityForResult(intent, Constants.requestCode.SELECT_FILE);
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)  {
-        switch (requestCode) {
-            case FILE_SELECT_CODE:
-                if (resultCode == RESULT_OK) {
-                    // Get the Uri of the selected file
-                    Uri uri = data.getData();
-                    String path = FileUtils.getPath(this, uri);
-                    Log.i("bingo",path);
-                }
-                break;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null) {
+            switch (requestCode) {
+                case Constants.requestCode.SELECT_FILE:
+                    String filePath = data.getStringExtra("filePath");
+                    tv_main_result.setText(filePath);
+                    break;
+            }
         }
-        super.onActivityResult(requestCode, resultCode, data);
     }
 }
