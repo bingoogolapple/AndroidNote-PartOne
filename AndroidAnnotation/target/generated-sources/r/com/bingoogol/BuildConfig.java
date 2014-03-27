b74b9394521e79ee593134367de852d95c18e772
bingoogol <bingoogol@sina.com>
2014-03-28 03:17:44 +0800

    init

---
+++ AndroidAnnotation/target/generated-sources/r/com/bingoogol/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.bingoogol;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
--- Fragment/src/com/bingoogol/fragment/demo3/JunShiFragment.java
+++ Fragment/src/com/bingoogol/fragment/demo3/JunShiFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import cn.bingoogol.pulltorefresh.PTRListView;
 import com.bingoogol.fragment.R;
 
 /**
