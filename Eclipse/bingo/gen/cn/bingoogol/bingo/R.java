18a810284d87f92679c6848eed176cc6a52bca21
bingoogol <bingoogol@sina.com>
2014-07-02 15:29:42 +0800

    添加dimens.xml

--- Eclipse/bingo/gen/cn/bingoogol/bingo/R.java
+++ Eclipse/bingo/gen/cn/bingoogol/bingo/R.java
@@ -10,61 +10,76 @@ package cn.bingoogol.bingo;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int iconSize_large=0x7f030002;
+        public static final int iconSize_middle=0x7f030001;
+        /**  公共的 START 
+         */
+        public static final int iconSize_small=0x7f030000;
+        public static final int size_divider=0x7f030003;
+        public static final int size_large=0x7f030006;
+        public static final int size_large_large=0x7f030007;
+        public static final int size_middle=0x7f030005;
+        public static final int size_small=0x7f030004;
+        public static final int textSize_large=0x7f03000a;
+        public static final int textSize_middle=0x7f030009;
+        public static final int textSize_small=0x7f030008;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
         /**  欢迎界面样式，修复第一次打开出现黑屏问题 
          */
-        public static final int Theme_Splash=0x7f040000;
+        public static final int Theme_Splash=0x7f050000;
         /**  宽度自适应，高度填充父窗体 
          */
-        public static final int auto_match=0x7f040006;
-        public static final int auto_match_horizontal=0x7f040016;
-        public static final int auto_match_vertical=0x7f040015;
+        public static final int auto_match=0x7f050006;
+        public static final int auto_match_horizontal=0x7f050016;
+        public static final int auto_match_vertical=0x7f050015;
         /**  宽度自适应，高度包裹内容 
          */
-        public static final int auto_wrap=0x7f040008;
-        public static final int auto_wrap_horizontal=0x7f04001a;
-        public static final int auto_wrap_vertical=0x7f040019;
+        public static final int auto_wrap=0x7f050008;
+        public static final int auto_wrap_horizontal=0x7f05001a;
+        public static final int auto_wrap_vertical=0x7f050019;
         /**  宽度填充父窗体，高度自适应 
          */
-        public static final int match_auto=0x7f040005;
-        public static final int match_auto_horizontal=0x7f040014;
-        public static final int match_auto_vertical=0x7f040013;
+        public static final int match_auto=0x7f050005;
+        public static final int match_auto_horizontal=0x7f050014;
+        public static final int match_auto_vertical=0x7f050013;
         /**  宽度和高度都填充父窗体 
          */
-        public static final int match_match=0x7f040004;
-        public static final int match_match_horizontal=0x7f040012;
-        public static final int match_match_vertical=0x7f040011;
+        public static final int match_match=0x7f050004;
+        public static final int match_match_horizontal=0x7f050012;
+        public static final int match_match_vertical=0x7f050011;
         /**  横线 
          */
-        public static final int match_one=0x7f040009;
+        public static final int match_one=0x7f050009;
         /**  宽度填充父窗体，高度包裹内容 
          */
-        public static final int match_wrap=0x7f040002;
-        public static final int match_wrap_horizontal=0x7f04000e;
-        public static final int match_wrap_vertical=0x7f04000d;
+        public static final int match_wrap=0x7f050002;
+        public static final int match_wrap_horizontal=0x7f05000e;
+        public static final int match_wrap_vertical=0x7f05000d;
         /**  竖线 
          */
-        public static final int one_match=0x7f04000a;
+        public static final int one_match=0x7f05000a;
         /**  宽度包裹内容，高度自适应 
          */
-        public static final int wrap_auto=0x7f040007;
-        public static final int wrap_auto_horizontal=0x7f040018;
-        public static final int wrap_auto_vertical=0x7f040017;
+        public static final int wrap_auto=0x7f050007;
+        public static final int wrap_auto_horizontal=0x7f050018;
+        public static final int wrap_auto_vertical=0x7f050017;
         /**  宽度包裹内容，高度 填充父窗体 
          */
-        public static final int wrap_match=0x7f040001;
-        public static final int wrap_match_horizontal=0x7f04000c;
-        public static final int wrap_match_vertical=0x7f04000b;
+        public static final int wrap_match=0x7f050001;
+        public static final int wrap_match_horizontal=0x7f05000c;
+        public static final int wrap_match_vertical=0x7f05000b;
         /**  宽度和高度都包裹内容 
          */
-        public static final int wrap_wrap=0x7f040003;
-        public static final int wrap_wrap_horizontal=0x7f040010;
-        public static final int wrap_wrap_vertical=0x7f04000f;
+        public static final int wrap_wrap=0x7f050003;
+        public static final int wrap_wrap_horizontal=0x7f050010;
+        public static final int wrap_wrap_vertical=0x7f05000f;
     }
 }
