d224922c6a94f0be33119214e8ca2b756629f6c2
bingoogol <bingoogol@sina.com>
2014-07-15 18:12:48 +0800

    修改打印的log

--- Eclipse/Tabhost/src/cn/bingoogol/tabhost/ui/fragment/AlbumFragment.java
+++ Eclipse/Tabhost/src/cn/bingoogol/tabhost/ui/fragment/AlbumFragment.java
@@ -1,27 +1,99 @@
 package cn.bingoogol.tabhost.ui.fragment;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
-import android.widget.TextView;
+import cn.bingoogol.tabhost.R;
+import cn.bingoogol.tabhost.util.Logger;
 
-public class AlbumFragment extends Fragment {  
-    
-    @Override  
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,  
-            Bundle savedInstanceState) {  
-        TextView textView = new TextView(getActivity());  
-        textView.setText("Album Fragment");  
-        textView.setGravity(Gravity.CENTER_HORIZONTAL);  
-        LinearLayout layout = new LinearLayout(getActivity());  
-        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);  
-        layout.addView(textView, params);  
-        return layout;  
-    }  
-  
-} 
+public class AlbumFragment extends Fragment {
+
+	private static final String TAG = AlbumFragment.class.getSimpleName();
+	private View mRootView;
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		Logger.i(TAG, "onAttach Album");
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Logger.i(TAG, "onCreate Album");
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		Logger.i(TAG, "onCreateView Album");
+		if (mRootView == null) {
+			Logger.i(TAG, "mRootView为空");
+			mRootView = inflater.inflate(R.layout.fragment_two, container, false);
+		} else {
+			Logger.i(TAG, "mRootView不为空");
+		}
+		ViewGroup parent = (ViewGroup) mRootView.getParent();
+		if (parent != null) {
+			parent.removeView(mRootView);
+		}
+		return mRootView;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		Logger.i(TAG, "onActivityCreated Album");
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		Logger.i(TAG, "onStart Album");
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		Logger.i(TAG, "onResume Album");
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		Logger.i(TAG, "onPause Album");
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		Logger.i(TAG, "onStop Album");
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		Logger.i(TAG, "onDestroyView Album");
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		Logger.i(TAG, "onSaveInstanceState Album");
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Logger.i(TAG, "onDestroy Album");
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		Logger.i(TAG, "onDetach Album");
+	}
+
+}
--- Eclipse/Tabhost/src/cn/bingoogol/tabhost/ui/fragment/ArtistFragment.java
+++ Eclipse/Tabhost/src/cn/bingoogol/tabhost/ui/fragment/ArtistFragment.java
@@ -1,26 +1,99 @@
 package cn.bingoogol.tabhost.ui.fragment;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
-import android.widget.TextView;
+import cn.bingoogol.tabhost.R;
+import cn.bingoogol.tabhost.util.Logger;
 
 public class ArtistFragment extends Fragment {
 
+	private static final String TAG = ArtistFragment.class.getSimpleName();
+	private View mRootView;
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		Logger.i(TAG, "onAttach Artist");
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Logger.i(TAG, "onCreate Artist");
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		TextView textView = new TextView(getActivity());
-		textView.setText("Artist Fragment");
-		textView.setGravity(Gravity.CENTER_HORIZONTAL);
-		LinearLayout layout = new LinearLayout(getActivity());
-		LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-		layout.addView(textView, params);
-		return layout;
+		Logger.i(TAG, "onCreateView Artist");
+		if (mRootView == null) {
+			Logger.i(TAG, "mRootView为空");
+			mRootView = inflater.inflate(R.layout.fragment_one, container, false);
+		} else {
+			Logger.i(TAG, "mRootView不为空");
+		}
+		ViewGroup parent = (ViewGroup) mRootView.getParent();
+		if (parent != null) {
+			parent.removeView(mRootView);
+		}
+		return mRootView;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		Logger.i(TAG, "onActivityCreated Artist");
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		Logger.i(TAG, "onStart Artist");
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		Logger.i(TAG, "onResume Artist");
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		Logger.i(TAG, "onPause Artist");
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		Logger.i(TAG, "onStop Artist");
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		Logger.i(TAG, "onDestroyView Artist");
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		Logger.i(TAG, "onSaveInstanceState Artist");
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Logger.i(TAG, "onDestroy Artist");
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		Logger.i(TAG, "onDetach Artist");
 	}
 
 }
