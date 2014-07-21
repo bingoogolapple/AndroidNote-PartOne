c914f77929a7438390363623540d4cdeb903ef1e
bingoogol <bingoogol@sina.com>
2014-07-21 17:32:52 +0800

    Add MainActivity

---
+++ Eclipse/bingo/src/cn/bingoogol/bingo/ui/activity/MainActivity.java
@@ -0,0 +1,25 @@
+package cn.bingoogol.bingo.ui.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import cn.bingoogol.bingo.R;
+
+public class MainActivity extends BaseActivity {
+
+	@Override
+	protected void initView(Bundle savedInstanceState) {
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void setListener() {
+	}
+
+	@Override
+	protected void afterViews(Bundle savedInstanceState) {
+	}
+
+	@Override
+	public void onClick(View v) {
+	}
+}
